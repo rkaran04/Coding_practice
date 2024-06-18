098f92b813ce2bba66102ec2eeb63b1433040de7
rkaran04 <103887972+rkaran04@users.noreply.github.com>
2024-06-18 19:53:52 +0530

    Create longest_valid_parenthesis.cpp

---
+++ Leetcode_questions/hard/longest_valid_parenthesis.cpp
@@ -0,0 +1,29 @@
+// once iterate from left and then from right. while iterating from left, keep a count of open and closed
+// if open=close, then valid, if close greater then reset both to 0. This will miss some strings. Do the 
+// opposite from right. there will be cases where this will not see few strings at all in both the cases, 
+// but it becoz there are better cases. 
+
+
+
+class Solution {
+public:
+
+    int longestValidParentheses(string s) {
+        int ans=0;
+        int open=0,close=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') open++;
+            else close++;
+            if(open==close) ans=max(ans,open+close);
+            if(close>open) open=close=0;
+        }
+        open=close=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='(') open++;
+            else close++;
+            if(open==close) ans=max(ans,open+close);
+            if(close<open) open=close=0;
+        }
+        return ans;
+    }
+};
